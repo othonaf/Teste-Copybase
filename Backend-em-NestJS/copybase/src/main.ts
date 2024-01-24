ad3f5016a4b045529eb299c1b7c30db6864106b8
othonaf <othon.ab@hotmail.com>
2024-01-24 16:22:34 -0300

    Atualizando

--- Backend-em-NestJS/copybase/src/main.ts
+++ Backend-em-NestJS/copybase/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://copybase-mtxc5ygd3-othons-projects.vercel.app/',
+    origin: 'https://copybase-mtxc5ygd3-othons-projects.vercel.app',
   });
   await app.listen(process.env.PORT || 3003);
 }
